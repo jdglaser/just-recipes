6e3f539bb14cb79f0b008f26cb116258af73dee1
jdglaser <jarred.glaser@gmail.com>
2021-11-24 10:50:27 -0600

    try one more time

--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -29,7 +29,6 @@ async function render(pageContext: PageContextBuiltIn & PageContext) {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="${logoUrl}" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
